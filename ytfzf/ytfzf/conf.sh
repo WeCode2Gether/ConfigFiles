acac4ff1b9103c356fab54e05075b917c4446d8d
wecode2gether <wecode2gether@gmail.com>
2025-01-25 10:39:54 +0300

    updated

---
+++ ytfzf/ytfzf/conf.sh
@@ -0,0 +1,18 @@
+invidious_instance="https://vid.puffyan.us"
+thumb_viewer="kitty"
+thumbnail_viewer="kitty"
+video_pref="1080"
+# Set the media player to VLC
+YT_DLP_MEDIA_PLAYER="vlc"
+
+# Variables
+player="vlc"
+ytdl_pref="248+bestaudio/best"
+show_thumbnails=1
+async_thumbnails=1
+is_audio_only=0
+is_detach=1
+format_selection_sort="resolution:desc"
+skip_thumb_download=0
+is_loop=1
+search_again=1
